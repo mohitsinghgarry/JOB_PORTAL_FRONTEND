1f9c2a7c739ab34d6ead4460e04aaf148d8130be
Mohit Singh <mohitsinghx4@gmail.com>
2024-08-24 16:14:57 +0530

    final-cmmit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,23 +21,8 @@ const Navbar = () => {
     <header className='max-w-screen container mx-auto xl:px-24 px-4'>
         <nav className="flex justify-between items-center py-6">
             <a href="/" className="flex items-center gap-2 text-2xl text-black">
-                <svg className="" 
-                width="29" 
-                height="30" 
-                viewBox="0 0 29 30"
-                xmlns="http://www.w3.org/2000/svg"
-                fill = "none"
-                >
-                    <circle
-                    cx= "12.0143"
-                    cy = "12.5143"
-                    r = "12.0143"
-                    fill = "#3575E2"
-                    fillOpacity = "0.4"
-                    />
-                    <circle cx = "16.9857" cy = "17.4857" r="12.0143" fill = "#3575E2" />
-                </svg>
-                <span className="">OpportunityOrbit</span>
+               
+                <span style={{fontWeight:"bold"}} className="">OpportunityOrbit</span>
             </a>
 
             {/* {NAV ITEMS FOR LARGE DEVICES} */}
